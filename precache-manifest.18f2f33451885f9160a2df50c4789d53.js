04798b299c69c8724a2aae686a31b79d0f6d1b10
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2024-03-31 13:37:19 -0400

    Updates

---
+++ precache-manifest.18f2f33451885f9160a2df50c4789d53.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3ddd267d813ec129791a14c4d4eb85",
+    "url": "/games/index.html"
+  },
+  {
+    "revision": "df3af7e60716bee59d57",
+    "url": "/games/static/css/main.236169a2.chunk.css"
+  },
+  {
+    "revision": "2a6c54bc11d3514c352b",
+    "url": "/games/static/js/2.1e20ddc5.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/games/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "df3af7e60716bee59d57",
+    "url": "/games/static/js/main.78767b45.chunk.js"
+  },
+  {
+    "revision": "4713d161f7d275987cf2",
+    "url": "/games/static/js/runtime~main.761cdb20.js"
+  },
+  {
+    "revision": "c1c6a15b0d8114a9438a5c217a06d5f1",
+    "url": "/games/static/media/SuperFunky.c1c6a15b.ttf"
+  },
+  {
+    "revision": "491adb262c3094fa4069ee4a5262d4ad",
+    "url": "/games/static/media/cat.491adb26.jpg"
+  },
+  {
+    "revision": "1651fb584f55dd563cdd872bd96e89d0",
+    "url": "/games/static/media/jailcat.1651fb58.jpg"
+  }
+]);
